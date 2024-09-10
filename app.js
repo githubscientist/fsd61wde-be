cc37cfebf9c5f7df13b1e1d6bd10b996f2870a47
githubscientist <sathish.datascientist@gmail.com>
2024-09-10 10:54:36 +0530

    register endpoint done

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const authRouter = require('./routes/authRoutes');
 
 const app = express();
 
+// middlewares
+// parse the incoming request with JSON payload
+app.use(express.json());
+
 // setup the routes
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');
-});
+app.use('/auth', authRouter);
 
 // export the app
 module.exports = app;
